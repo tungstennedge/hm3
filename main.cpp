ffd1bfdce351ab702125490bbfc3def8d0e8d1d4
tungstennedge <54876417+tungstennedge@users.noreply.github.com>
2020-11-06 19:11:37 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,94 +1,44 @@
 #include <iostream>
-#include "ArrayList.h"
-#include <iterator>
-static void testArrayListUnderflow()
-{
-    try{
-        ArrayList<int> arr;
-        arr = ArrayList<int>();
-        arr.insertBack(1);
-        arr.removeFront();
-        arr.removeFront();
-
-        cout <<"did not catch exception"<< "\n";
-    }catch(EmptyListException){
-        cout << "caught Empty Stack Excpetion" << "\n";
+#include "Entry.h"
+#include "Heap.h"
+using namespace std;
+void printEntires(Entry* arr, int n){
+    cout <<"Printing Entries, (key, string) "<< "\n\n";
+    for (int i = 0; i < n; ++i) {
+        cout << "(" << arr[i].getKey();
+        cout << ", " << arr[i].getData() << ")"<< "\n";
     }
-    try{
-        ArrayList<int> arr;
-        arr = ArrayList<int>();
-        arr.insertBack(1);
-        arr.removeBack();
-        arr.removeBack();
-
-        cout <<"did not catch exception"<< "\n";
-    }catch(EmptyListException){
-        cout << "caught Empty Stack Excpetion" << "\n";
-    }
-    try{
-        ArrayList<int> arr;
-        arr = ArrayList<int>();
-        arr.insertBack(1);
-        ArrayList<int>::Iterator itr(0,arr);
-        itr = arr.begin();
-        arr.remove(itr);
-        arr.remove(itr);
-
-        cout <<"did not catch exception"<< "\n";
-    }catch(EmptyListException){
-        cout << "caught Empty Stack Excpetion" << "\n";
-    }
-
-
-
 }
-void testIntegerIterator(){
-    ArrayList<int> arr;
-    arr = ArrayList<int>();
-
+void printEntries(Entry e){
+    cout << "(" << e.getKey();
+    cout << ", " << e.getData() << ")"<< "\n";
 }
-
-void testStringIterator(){
-
-}
-template<typename T>
-void printArr(ArrayList<T> L){
-
-    for(typename ArrayList<T>::Iterator iter = L.begin(); !(iter.operator==(L.end())); iter.operator++()) {
-        cout << iter.operator*() << " ";
+void heapsort2(Heap h, int n, Entry* entries){
+    for (int i = 0; i < n; ++i) {
+        cout << "removeing entry: ";
+        entries[i] = h.removeMax();
     }
-    cout << endl;
+}
+void populateEntries(int n, Entry* entries){
+    for (int i = 0; i < n; ++i) {
+        Entry e;
+        e.random();
+        entries[i] = e;
+    }
 }
 int main() {
+    int n = 2;
     std::cout << "Hello, World!" << std::endl;
-    ArrayList<int> arr;
-    arr = ArrayList<int>();
 
+    Entry* entries = new Entry[n];
+    populateEntries(n,entries);
+    printEntires(entries,n);
 
-
-    for (int i = 0; i < 9; ++i) {
-       arr.insertBack(1);
-    }
-    ArrayList<int>::Iterator itr = arr.begin();
-    ArrayList<int>::Iterator itr2 = arr.end();
-    cout << "itr val: " << itr.operator*()<<"\n";
-    cout << "itr val2: " << itr2.operator*()<<"\n";
-
-    if(itr2.operator==(arr.end())){
-        cout << " iterator operator == is working"<< "\n";
-    }else{
-        cout << "nope";
-    }
-
-
-   arr.print_arr(); // working printr
-   printArr(arr); // iterator printer
-    cout <<"\n";
-    //cout<< "printing arr[itr]: " << itr.operator*()<< "\n";
-    testArrayListUnderflow();
-
+    Heap h = Heap();
+    h.make(entries,n);
+    Entry e = h.removeMax();
+    printEntries(e);
+    Entry d = h.removeMax();
+    printEntries(d);
     return 0;
 }
-
-
-
